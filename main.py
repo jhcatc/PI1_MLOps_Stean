2d2daac174214d67fc2c42ea645e8cbd37450dad
jhcatc <jhcatc@gmail.com>
2024-05-03 21:43:31 -0500

    recomendaciones 3

--- main.py
+++ main.py
@@ -146,5 +146,6 @@ ________________________________________________________________________________
 ''' 
 # Punto de entrada de la aplicación
 if __name__ == "__main__":
+    import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=10000)
 
